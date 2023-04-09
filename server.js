163d9fbc932bf7ef75452003b4568da3c8a1b6c4
hashimshah47 <hashimtayyab47@gmail.com>
2023-04-09 13:10:31 +0500

    cors added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import fetch from "node-fetch";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 // const path = require('path');
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API endpoint to retrieve data from external API
